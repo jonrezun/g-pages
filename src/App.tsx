a1dc9243b7be7ce636a5db5737e29ae5f15434e1
jonrezun <jonrezun@gmail.com>
2024-03-16 23:36:05 +0300

    react

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import './App.scss'
+
+const App = () => {
+
+  return (
+    <>
+        <section className="hero container-fluid">
+            <div className="hero__inner container">
+                <header className="hero__header">
+                    <a href="/" className="hero__logo">REZUN PRODUCTION</a>
+                    <button className="hero__burger"></button>
+                </header>
+                <div className="hero__main">
+                    <h1 className="hero__title">Frontend-разработкка для e-commerce!</h1>
+                    <p>10 лет помогаю eccome решать бизнес-задачи и достигать новых высот. Маркетинг и технологии в симбиозе.</p>
+                </div>
+                <ul className="hero__brands">
+                    {/*<li className="hero_brand">brand</li>*/}
+                </ul>
+            </div>
+        </section>
+    </>
+  )
+}
+
+export default App
